dc88af50ac3d8ef1e7b41799364c6b8cf27ef877
Alexey Volkov <alexey@pison.com>
2024-07-17 23:06:26 +0200

    Implement appearing/disappearing shader

--- app/src/main/java/com/offmind/runtimeshaders/MainActivity.kt
+++ app/src/main/java/com/offmind/runtimeshaders/MainActivity.kt
@@ -5,19 +5,19 @@ import android.graphics.RuntimeShader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
@@ -25,15 +25,16 @@ import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.offmind.runtimeshaders.composables.provideTimeAsState
-import com.offmind.runtimeshaders.shaders.initial_shader
+import com.offmind.runtimeshaders.shaders.disappear_shader
 import com.offmind.runtimeshaders.ui.theme.RuntimeShadersTheme
 import kotlin.random.Random
 
-class MainActivity : ComponentActivity() {
+class MainActivity() : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var stage by remember { mutableFloatStateOf(0f) }
             RuntimeShadersTheme {
                 Box(
                     modifier = Modifier
@@ -45,12 +46,12 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         painter = painterResource(id = R.drawable.generic_background),
                         contentDescription = null,
-                        contentScale = ContentScale.Crop
-                    )
+                        contentScale = ContentScale.Crop)
                     ShaderView(
-                        modifier = Modifier
-                            .size(250.dp)
-                            .clipToBounds(),
+                        modifier = Modifier.size(250.dp).clickable {
+                            stage = if (stage == 1f) 0f else 1f
+                        },
+                        animStage = stage,
                         content = {
                             Image(
                                 modifier = Modifier.fillMaxSize(),
@@ -69,12 +70,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ShaderView(
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
+    animStage: Float = 0f
 ) {
     val shader by remember {
-        mutableStateOf(RuntimeShader(initial_shader))
+        mutableStateOf(RuntimeShader(disappear_shader))
     }
     val time by provideTimeAsState(Random.nextFloat() * 10f)
+
+    val anim = animateFloatAsState(
+        targetValue = animStage,
+        tween(durationMillis = 2000, easing = FastOutSlowInEasing),
+        label = ""
+    )
 
     Box(modifier = modifier
         .onSizeChanged { size ->
@@ -86,6 +94,7 @@ fun ShaderView(
         }
         .graphicsLayer {
             shader.setFloatUniform("time", time)
+            shader.setFloatUniform("percentage", anim.value)
             this.renderEffect = RenderEffect
                 .createRuntimeShaderEffect(shader, "image")
                 .asComposeRenderEffect()
--- app/src/main/java/com/offmind/runtimeshaders/shaders/Shaders.kt
+++ app/src/main/java/com/offmind/runtimeshaders/shaders/Shaders.kt
@@ -1,17 +1,52 @@
 package com.offmind.runtimeshaders.shaders
 
-val initial_shader = """
+val disappear_shader = """
         uniform shader image;
         uniform vec2 resolution;
         uniform float time;
-       
+        uniform float percentage;
+          
+        float rand(vec2 c){
+            return fract(sin(dot(c.xy ,vec2(12.9898,78.233))) * 43758.5453);
+        }
+
+        float noise(vec2 p, float freq){
+            float unit = resolution.y/freq;
+            vec2 ij = floor(p/unit);
+            vec2 xy = mod(p,unit)/unit;
+            //xy = 3.*xy*xy-2.*xy*xy*xy;
+            xy = .5*(1.-cos(3.1415*xy));
+            float a = rand((ij+vec2(0.,0.)));
+            float b = rand((ij+vec2(1.,0.)));
+            float c = rand((ij+vec2(0.,1.)));
+            float d = rand((ij+vec2(1.,1.)));
+            float x1 = mix(a, b, xy.x);
+            float x2 = mix(c, d, xy.x);
+            return mix(x1, x2, xy.y);
+        }
+            
         vec4 main(float2 fragCoord) {
-            float2 uv = fragCoord / resolution - 0.5;
-           
-            vec3 col = vec3(
-                abs(sin(cos(time+3.*uv.y)*2.*uv.x+time)),
-                abs(cos(sin(time+2.*uv.x)*3.*uv.y+time)),
-                abs(cos(cos(time+2.*uv.x)*3.*uv.y+time)));
-            return vec4(col, 1.0);
-       }
+         float2 uv = fragCoord / resolution - 0.5;
+         float2 sv = uv * (1.-percentage*0.5);
+        
+         float scale = (1.-percentage*0.2)-(length(uv)*percentage)*.6;
+         uv *= scale;
+         float n =step(percentage,noise(uv,20000.));
+         
+         vec2 scaledFragCoords = fragCoord*scale;
+         vec2 translation = vec2(
+            0.5*(resolution.x-resolution.x*scale), 
+            0.5*(resolution.y-resolution.y*scale)
+            );
+         
+         vec4 parent = image.eval(scaledFragCoords+translation);
+         vec3 col = parent.rgb*n;
+         
+         float borders = smoothstep(.5,0.48,length(sv.x))* smoothstep(.5,0.48,length(sv.y));
+         col*=borders;
+         
+         float alpha = 1.0-percentage;//1.-percentage*length(uv*(1.+percentage));
+                  
+         return vec4(col*parent.a, parent.a*alpha*borders);
+        }
 """.trimIndent()
