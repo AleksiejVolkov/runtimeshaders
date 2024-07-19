8d9701f5b37bb8f94d1ce8359ecf1a4177ce8e93
Alexey Volkov <alexey@pison.com>
2024-07-19 14:28:52 +0200

    Implement text to noise shader

--- app/src/main/java/com/offmind/runtimeshaders/MainActivity.kt
+++ app/src/main/java/com/offmind/runtimeshaders/MainActivity.kt
@@ -2,30 +2,33 @@ package com.offmind.runtimeshaders
 
 import android.graphics.RenderEffect
 import android.graphics.RuntimeShader
+import android.health.connect.datatypes.units.Percentage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.offmind.runtimeshaders.composables.provideTimeAsState
-import com.offmind.runtimeshaders.shaders.initial_shader
+import com.offmind.runtimeshaders.shaders.text_from_noise
 import com.offmind.runtimeshaders.ui.theme.RuntimeShadersTheme
 import kotlin.random.Random
 
@@ -37,28 +40,26 @@ class MainActivity : ComponentActivity() {
             RuntimeShadersTheme {
                 Box(
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.background)
+                        .background(Color.White)
                         .fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        painter = painterResource(id = R.drawable.generic_background),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop
-                    )
+                    var percentage by remember {
+                        mutableFloatStateOf(0.0f)
+                    }
+
+                    val percentageAnim = animateFloatAsState(targetValue = percentage, label = "")
+
                     ShaderView(
                         modifier = Modifier
-                            .size(250.dp)
-                            .clipToBounds(),
+                            .clipToBounds()
+                            .clickable {
+                                percentage = if (percentage == 0.0f) 1.0f else 0.0f
+                            },
                         content = {
-                            Image(
-                                modifier = Modifier.fillMaxSize(),
-                                painter = painterResource(id = R.drawable.generic_image),
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop
-                            )
-                        }
+                            Text(text = "Hello, World!", color = Color.Black, fontSize = 24.sp)
+                        },
+                        percentage = percentageAnim.value
                     )
                 }
             }
@@ -69,10 +70,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ShaderView(
     modifier: Modifier = Modifier,
+    percentage: Float = 0.5f,
     content: @Composable () -> Unit
 ) {
     val shader by remember {
-        mutableStateOf(RuntimeShader(initial_shader))
+        mutableStateOf(RuntimeShader(text_from_noise))
     }
     val time by provideTimeAsState(Random.nextFloat() * 10f)
 
@@ -86,6 +88,7 @@ fun ShaderView(
         }
         .graphicsLayer {
             shader.setFloatUniform("time", time)
+            shader.setFloatUniform("percentage", percentage)
             this.renderEffect = RenderEffect
                 .createRuntimeShaderEffect(shader, "image")
                 .asComposeRenderEffect()
--- app/src/main/java/com/offmind/runtimeshaders/shaders/Shaders.kt
+++ app/src/main/java/com/offmind/runtimeshaders/shaders/Shaders.kt
@@ -1,17 +1,26 @@
 package com.offmind.runtimeshaders.shaders
 
-val initial_shader = """
+val text_from_noise = """
         uniform shader image;
         uniform vec2 resolution;
         uniform float time;
-       
+        uniform float percentage;
+        
+        float Hash21(vec2 p) { 
+            return fract(sin(dot(p, vec2(12.9898, 78.233))) * 43758.5453);
+        }
+            
         vec4 main(float2 fragCoord) {
-            float2 uv = fragCoord / resolution - 0.5;
-           
-            vec3 col = vec3(
-                abs(sin(cos(time+3.*uv.y)*2.*uv.x+time)),
-                abs(cos(sin(time+2.*uv.x)*3.*uv.y+time)),
-                abs(cos(cos(time+2.*uv.x)*3.*uv.y+time)));
-            return vec4(col, 1.0);
-       }
+            vec4 parent = image.eval(fragCoord);
+            vec2 uv = fragCoord / resolution - 0.5;
+            vec2 shiftedUv = uv + time*uv;
+            
+            vec3 noise = vec3(Hash21(shiftedUv)); // for now just black
+            vec3 image = parent.rgb;
+            
+            vec3 col = mix(noise, image, percentage);
+            float a = mix(1.0, parent.a, percentage);
+            
+            return vec4(col, a);
+        }
 """.trimIndent()
