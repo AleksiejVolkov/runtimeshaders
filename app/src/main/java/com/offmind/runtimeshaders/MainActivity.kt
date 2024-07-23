c440f8ac462e5f5f62b101e7751d00a33804820e
Alexey Volkov <alexey@pison.com>
2024-07-23 10:56:36 +0200

    Split shader text into different blocks

--- app/src/main/java/com/offmind/runtimeshaders/MainActivity.kt
+++ app/src/main/java/com/offmind/runtimeshaders/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,12 +17,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.offmind.runtimeshaders.composables.provideTimeAsState
 import com.offmind.runtimeshaders.shaders.initial_shader
 import com.offmind.runtimeshaders.ui.theme.RuntimeShadersTheme
@@ -41,20 +40,14 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        painter = painterResource(id = R.drawable.generic_background),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop
-                    )
                     ShaderView(
                         modifier = Modifier
-                            .size(250.dp)
+                            .fillMaxSize()
                             .clipToBounds(),
                         content = {
                             Image(
                                 modifier = Modifier.fillMaxSize(),
-                                painter = painterResource(id = R.drawable.generic_image),
+                                painter = painterResource(id = R.drawable.generic_nature_1),
                                 contentDescription = null,
                                 contentScale = ContentScale.Crop
                             )
@@ -69,7 +62,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ShaderView(
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit = {}
 ) {
     val shader by remember {
         mutableStateOf(RuntimeShader(initial_shader))
@@ -90,6 +83,10 @@ fun ShaderView(
                 .createRuntimeShaderEffect(shader, "image")
                 .asComposeRenderEffect()
         }) {
-        content()
+        Box(modifier = Modifier.fillMaxSize().background(color = Color.Black.copy(alpha = 0.1f)))
+        {
+            content()
+        }
+
     }
 }
--- app/src/main/java/com/offmind/runtimeshaders/shaders/Shaders.kt
+++ app/src/main/java/com/offmind/runtimeshaders/shaders/Shaders.kt
@@ -1,17 +1,55 @@
 package com.offmind.runtimeshaders.shaders
 
-val initial_shader = """
+/**
+ * Retrieves the color of the image at a specified position, adjusted by a pivot point.
+ *
+ * This function first adjusts the y-coordinate of the position `p` to maintain the aspect ratio
+ * of the image based on the `resolution`. It then applies a pivot transformation to `p`, scales
+ * the position by the `resolution`, and finally samples the color of the image at this adjusted
+ * position.
+ *
+ * @param p The original position vector from which to sample the image, typically representing UV coordinates.
+ * @param pivot A vector representing the pivot point used to adjust the position `p` before sampling.
+ * @return The color of the image at the adjusted position as a `vec4`.
+ */
+val GetViewTextureFunction = """
+        vec4 GetImageTexture(vec2 p, vec2 pivot) {
+          p.y /= resolution.y / resolution.x;
+          p+=pivot;
+          p*=resolution;
+          return image.eval(p);
+        }   
+""".trimIndent()
+
+val Uniforms = """
         uniform shader image;
         uniform vec2 resolution;
         uniform float time;
-       
-        vec4 main(float2 fragCoord) {
+""".trimIndent()
+
+val Functions = """
+        $GetViewTextureFunction
+""".trimIndent()
+
+val initial_shader = Uniforms + Functions + """
+       vec4 main(float2 fragCoord) {
             float2 uv = fragCoord / resolution - 0.5;
-           
-            vec3 col = vec3(
-                abs(sin(cos(time+3.*uv.y)*2.*uv.x+time)),
-                abs(cos(sin(time+2.*uv.x)*3.*uv.y+time)),
-                abs(cos(cos(time+2.*uv.x)*3.*uv.y+time)));
-            return vec4(col, 1.0);
+            uv.y *= resolution.y / resolution.x;
+            vec3 checker = GetImageTexture(uv, vec2(0.5, 0.5)).rgb;
+            
+            uv.y += 0.9*sin(time);
+            float circle = smoothstep(0.3,0.29,length(uv));
+            float circle2 = smoothstep(0.29,0.28,length(uv));
+
+            vec3 upsideDownChecker = GetImageTexture(vec2(-1.)*uv*length(uv)*4.,vec2(0.5,0.5+0.3*sin(time))).rgb; 
+            
+            upsideDownChecker *= vec3(0.9);
+            upsideDownChecker += vec3(1.)*0.2*smoothstep(.8,0.,length(uv+0.2));
+            vec3 finlcol = circle*upsideDownChecker;
+             
+            vec3 col2 = mix(checker,finlcol,circle2);
+            
+            return vec4(col2, 1.0);
        }
 """.trimIndent()
+
