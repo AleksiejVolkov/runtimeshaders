4cded59eb77a98bb854b0abaab7fa376708a96e3
sergey <sergey.sverchkov@fieldbee.com>
2024-12-26 22:44:09 +0100

    added snow flakes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     kotlin("plugin.serialization") version "2.0.20"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
 }
 
 android {
